899846cceb0e4967a934c6f16c79fc0d321112b0
samson omondi <seth@localhost.localdomain>
2019-12-20 12:53:56 +0300

    edit webpack env file

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'development') {
     require('dotenv').config({ path: '.env.development' });
-} else if (process.env.NODE_ENV === 'production') {
-    require('dotenv').config({ path: '.env.production' });
 }
 
 module.exports = (env) => {
