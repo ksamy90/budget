dc429ce2221a2ce4fa0fc08d1775077b19c1e284
samson omondi <seth@localhost.localdomain>
2019-12-20 15:32:13 +0300

    edit gitignore firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 var firebaseConfig = {
     apiKey: "AIzaSyDLdxdIMqlsg0Zj7loTkF7cQM2MjhC9Rfg",
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    authDomain: "design-454c3.firebaseapp.com",
     databaseURL: "https://design-454c3.firebaseio.com",
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    projectId: "design-454c3",
+    storageBucket: "design-454c3.appspot.com",
+    messagingSenderId: "1038180746876",
+    appId: "1:1038180746876:web:410343e6cd4241f0"
 };
 
 firebase.initializeApp(firebaseConfig);
